320d3d28e42b195eb3673689f3a4e712d4bdeffc
Kay Sharrock <sharrock.kay@gmail.com>
2023-03-13 13:33:05 +0000

    Edit test naming to match content

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders app title element", () => {
   render(<App />);
   const linkElement = screen.getByText(/TechReturners Casino/i);
   expect(linkElement).toBeInTheDocument();

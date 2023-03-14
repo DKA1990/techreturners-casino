bb61b3960b0595a261e79a926cf03879ea2639c6
danielatwell2k5@msn.com <danielatwell2k5@msn.com>
2023-03-14 12:29:14 +0000

    feat: Additional tests for initial hand. Refactoring draw cards method for ease in future uses.

--- server/src/controllers/start-game-controller.test.ts
+++ server/src/controllers/start-game-controller.test.ts
@@ -75,3 +75,113 @@ test('GET /startgame should return json containing two cards and game state "INP
         "stateOfGame": "INPLAY"
     });
 });
+
+test('GET /startgame should return json containing two cards and game state "BLACKJACK"', async () => {
+    cardServer.use(
+        rest.get('https://deckofcardsapi.com/api/deck/dvjw5ozpn8h4/draw/', (req, res, ctx) => {
+            return res(ctx.json({
+                "success": true,
+                "deck_id": "dvjw5ozpn8h4",
+                "cards": [
+                    {
+                        "code": "AC",
+                        "image": "https://deckofcardsapi.com/static/img/AC.png",
+                        "images": {
+                            "svg": "https://deckofcardsapi.com/static/img/AC.svg",
+                            "png": "https://deckofcardsapi.com/static/img/AC.png"
+                        },
+                        "value": "ACE",
+                        "suit": "CLUBS"
+                    },
+                    {
+                        "code": "KD",
+                        "image": "https://deckofcardsapi.com/static/img/KD.png",
+                        "images": {
+                            "svg": "https://deckofcardsapi.com/static/img/KD.svg",
+                            "png": "https://deckofcardsapi.com/static/img/KD.png"
+                        },
+                        "value": "KING",
+                        "suit": "DIAMONDS"
+                    }
+                ],
+                "remaining": 50
+            }));
+        })
+    );
+
+    const res = await request(app).get('/startgame');
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+        cards: [
+            {
+                "value": "ACE",
+                "suit": "CLUBS",
+                "pointValue": 11,
+                "image": "https://deckofcardsapi.com/static/img/AC.png"
+            },
+            {
+                "value": "KING",
+                "suit": "DIAMONDS",
+                "pointValue": 10,
+                "image": "https://deckofcardsapi.com/static/img/KD.png"
+            }
+        ],
+        "stateOfGame": "BLACKJACK"
+    });
+});
+
+test('If GET /startgame returns json containing two cards which are both aces, game state should return "INPLAY"', async () => {
+    cardServer.use(
+        rest.get('https://deckofcardsapi.com/api/deck/dvjw5ozpn8h4/draw/', (req, res, ctx) => {
+            return res(ctx.json({
+                "success": true,
+                "deck_id": "dvjw5ozpn8h4",
+                "cards": [
+                    {
+                        "code": "AC",
+                        "image": "https://deckofcardsapi.com/static/img/AC.png",
+                        "images": {
+                            "svg": "https://deckofcardsapi.com/static/img/AC.svg",
+                            "png": "https://deckofcardsapi.com/static/img/AC.png"
+                        },
+                        "value": "ACE",
+                        "suit": "CLUBS"
+                    },
+                    {
+                        "code": "AD",
+                        "image": "https://deckofcardsapi.com/static/img/AD.png",
+                        "images": {
+                            "svg": "https://deckofcardsapi.com/static/img/AD.svg",
+                            "png": "https://deckofcardsapi.com/static/img/AD.png"
+                        },
+                        "value": "ACE",
+                        "suit": "DIAMONDS"
+                    }
+                ],
+                "remaining": 50
+            }));
+        })
+    );
+
+    const res = await request(app).get('/startgame');
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+        cards: [
+            {
+                "value": "ACE",
+                "suit": "CLUBS",
+                "pointValue": 11,
+                "image": "https://deckofcardsapi.com/static/img/AC.png"
+            },
+            {
+                "value": "ACE",
+                "suit": "DIAMONDS",
+                "pointValue": 1,
+                "image": "https://deckofcardsapi.com/static/img/AD.png"
+            }
+        ],
+        "stateOfGame": "INPLAY"
+    });
+});
--- server/src/services/draw-cards.ts
+++ server/src/services/draw-cards.ts
@@ -1,30 +1,45 @@
 import { getDeckId } from "./current-state";
 import { Card, PointValue, Value } from "../types/game-types";
 
-export async function drawCards(numOfCards: 1 | 2) {
+export async function drawCards(numOfCards: 1 | 2, hand?: Card[]) {
     const drawnCards: Card[] = [];
+    let valueTotal: number;
+    if (hand) {
+        valueTotal = hand.reduce((acc, cur) => acc + cur.pointValue, 0); 
+    } else {
+        valueTotal = 0;
+    }
+
     const apiDraw = await fetch (`https://deckofcardsapi.com/api/deck/${getDeckId()}/draw/?count=${numOfCards}`);
     const json = await apiDraw.json();
     if (json.success === true) {
         json.cards.forEach((card: Card) => {
-            const pointValue = (val: Value) : PointValue => {
-                switch (val) {
-                    case "ACE":
-                        return 11;
-                    case "KING":
-                        return 10;
-                    case "QUEEN":
-                        return 10;
-                    case "JACK":
-                        return 10;
-                    default:
-                        return parseInt(val) as PointValue;
-                }
-            };
+            let value: PointValue;
+
+            switch (card.value) {
+                case "ACE":
+                    (valueTotal + 11) > 21 ? value = 1 : value = 11;
+                    break;
+                case "KING":
+                    value = 10;
+                    break;
+                case "QUEEN":
+                    value = 10;
+                    break;
+                case "JACK":
+                    value = 10;
+                    break;
+                default:
+                    value = parseInt(card.value) as PointValue;
+                    break;
+            }
+
+            valueTotal = valueTotal + value;
+
             drawnCards.push({
                 value: card.value,
                 suit: card.suit,
-                pointValue: pointValue(card.value),
+                pointValue: value,
                 image: card.image
             });            
         });

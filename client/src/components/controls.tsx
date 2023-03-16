16b3b292da21ab30ad66d25196ab198db8855a1e
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-03-16 13:03:18 +0000

    feat: created hit game control

--- client/src/components/controls.tsx
+++ client/src/components/controls.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useGame } from "../context/game_provider";
+import { HitGameControl } from './hitGameControl';
 import { StartGameControl } from './startGameControl';
 
 export const Controls : React.FC = () => {
@@ -8,6 +9,7 @@ export const Controls : React.FC = () => {
     return (
         <div>
             {stateOfGame === "new" && <StartGameControl />}
+            {stateOfGame === "INPLAY" && <HitGameControl />}
             {/* stateOfGame === "INPLAY" && <InPlay controls />*/}
             {/* stateOfGame === "Standing" && <Standing controls />*/}
             {/* stateOfGame === "BlackJack" && <BlackJack controls />*/}

f9fb8864b823f37b7237b457540eaf9aa27d1454
danielatwell2k5@msn.com <danielatwell2k5@msn.com>
2023-03-16 12:40:44 +0000

    feat: Blackjack rules component. Ability to show / hide

--- client/src/components/rule.tsx
+++ client/src/components/rule.tsx
@@ -1,27 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import AppButton from "./AppButton";
 
 export const Rules : React.FC = () => {
+    const [showRules, setShowRules] = useState(false);
+
+    const handleRules = () => {
+        showRules ? setShowRules(false) : setShowRules(true);
+    }
 
     return (
         <section className="blackjack-rules">
-            <h2>BlackJack Rules</h2>
-            <p>
-                1. Blackjack starts with the player being dealt two cards.
-            </p>
-            <p>
-                2. All cards are considered as their face value in Blackjack. Picture cards (Jacks, Queens and Kings)
-                all have a value of 10. Aces count as 11 unless the player would bust, in which case they are counted
-                as 1.
-            </p>
-            <p>
-                3. The aim of the for the player is to get their hand value to 21, or as close to 21 and possible without
-                going over.
-            </p>
-            <p>
-                4. In the case of the value of player's hand exceeding 21, they lose the game. At the moment this is
-                inconsequential but they should be ashamed!
-            </p>
-            <img className="blackjack-rules__img" src="../../images/blackjack-card-values.png"/>
+            <AppButton onClick={handleRules}>Show Rules</AppButton>
+            {showRules && 
+            <>
+                <h2>BlackJack Rules</h2>
+                <p>
+                    1. Blackjack starts with the player being dealt two cards.
+                </p>
+                <p>
+                    2. All cards are considered as their face value in Blackjack. Picture cards (Jacks, Queens and Kings)
+                    all have a value of 10. Aces count as 11 unless the player would bust, in which case they are counted
+                    as 1.
+                </p>
+                <p>
+                    3. The aim of the for the player is to get their hand value to 21, or as close to 21 and possible without
+                    going over.
+                </p>
+                <p>
+                    4. In the case of the value of player's hand exceeding 21, they lose the game. At the moment this is
+                    inconsequential but they should be ashamed!
+                </p>
+                <img className="blackjack-rules__img" src={require("../images/blackjack-card-values.png")}/>
+            </>
+            }   
         </section>
     )
 }

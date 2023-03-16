89189ed8f5427470a3584874865f42471b8a579a
danielatwell2k5@msn.com <danielatwell2k5@msn.com>
2023-03-16 17:24:42 +0000

    Added additional WIN, LOSE and DRAW states. Dealer hand and logic surrounding

--- server/src/services/stand.ts
+++ server/src/services/stand.ts
@@ -1,10 +1,20 @@
 import { hasBust } from "./bust";
-import { getGameState, getPlayerHand, setGameState } from "./current-state";
+import { getDealerHand, getGameState, getPlayerHand, setGameState } from "./current-state";
+import { drawDealerCards } from "./draw-cards";
 
 export async function stand() {
-  hasBust(getPlayerHand()) ? setGameState("BUST") : setGameState("STANDING");
-  return {
-    cards: getPlayerHand(),
-    stateOfGame: getGameState(),
-  };
+    hasBust(getPlayerHand()) ? setGameState("BUST") : setGameState("STANDING");
+    if (getGameState() !== "BUST") {
+        await drawDealerCards();
+        const playerValue = getPlayerHand().reduce((acc, cur) => acc + cur.pointValue, 0);
+        const dealerValue = getDealerHand().reduce((acc, cur) => acc + cur.pointValue, 0);
+        (playerValue > dealerValue) ? setGameState("WIN") : 
+            (playerValue < dealerValue) ? setGameState("LOSE") : setGameState("DRAW");
+    }
+
+    return {
+        cards: getPlayerHand(),
+        stateOfGame: getGameState(),
+        dealerCards: getDealerHand()
+    };
 }

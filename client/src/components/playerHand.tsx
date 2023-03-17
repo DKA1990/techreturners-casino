1701e64987b2ef69d537f565eceea7b9e1a91fe3
ilfa <ilfahassan@gmail.com>
2023-03-17 23:33:39 +0000

    included the playerHand calculation to playerHand component

--- client/src/components/playerHand.tsx
+++ client/src/components/playerHand.tsx
@@ -4,9 +4,28 @@ import { Card } from "../types/game-types";
 interface HandProps {
   cards: Card[];
 }
+const calculateTotal = (cards: Card[]) => {
+  let total = 0;
+  let hasAce = false;
+  for (let card of cards) {
+    if (card.pointValue === 1) {
+      hasAce = true;
+    }
+    total += card.pointValue;
+  }
+  if (hasAce && total + 10 <= 21) {
+    total += 10;
+  }
+  return total;
+};
 
 const PlayerHand: React.FC<HandProps> = ({ cards }) => {
-  return <div>{cards.map((card, index) => card.pointValue)}</div>;
+  return (
+    <div>
+      {cards.map((card, index) => card.value)}
+      <div>Total: {calculateTotal(cards)}</div>
+    </div>
+  );
 };
 
 export default PlayerHand;

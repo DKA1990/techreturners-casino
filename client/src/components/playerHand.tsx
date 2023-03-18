97941aa4a59e5667dc1fa4c72cd92dcd20f26b74
ilfa <ilfahassan@gmail.com>
2023-03-18 11:08:24 +0000

    Refactored playerHand

--- client/src/components/playerHand.tsx
+++ client/src/components/playerHand.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 import { Card } from "../types/game-types";
 import { useGame } from "../context/game_provider";
+import DisplayCard from "./DisplayCard";
 
 const calculateTotal = (cards: Card[]) => {
   let total = 0;
-  let hasAce = false;
+
   for (let card of cards) {
-    if (card.pointValue === 1) {
-      hasAce = true;
-    }
     total += card.pointValue;
-  }
-  if (hasAce && total + 10 <= 21) {
-    total += 10;
   }
   return total;
 };
@@ -21,8 +16,10 @@ const PlayerHand: React.FC = () => {
   const { cards } = useGame();
   return (
     <div>
-      {cards.map((card) => card.image)}
-      <div>Total: {calculateTotal(cards)}</div>
+      {cards.map((card) => (
+        <DisplayCard card={card} />
+      ))}
+      <div>Player Hand Total: {calculateTotal(cards)}</div>
     </div>
   );
 };

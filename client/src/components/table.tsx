ea02f9500789a49637416b81c9b1e930ad0913b1
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-03-18 10:05:06 +0000

    feat: update table to show dealer cards

--- client/src/components/table.tsx
+++ client/src/components/table.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { useGame } from "../context/game_provider";
 import { Controls } from "./controls";
+import DisplayCard from "./DisplayCard";
 
 export const Table: React.FC = () => {
-  const { stateOfGame, cards } = useGame();
+  const { stateOfGame, cards, dealerCards } = useGame();
 
   console.log(`stateGame is ${stateOfGame}`);
 
   return (
     <main className="table">
+        <div>
+          {dealerCards.length >= 1 && <p>DealerCards</p>}
+          {dealerCards.map((card, index) => (
+            <DisplayCard key={index} card={card} />
+          ))}
+      </div>
       <Controls />
       {/** The following code is placeholder and needs to be refactored into a hand component */}
       {/** TO DO: loading and error states to be considered */}
@@ -16,7 +23,7 @@ export const Table: React.FC = () => {
         <p>{stateOfGame}</p>
         {cards.length >= 1 && <p>Cards</p>}
         {cards.map((card, index) => (
-          <p key={index}>{card.suit}</p>
+          <DisplayCard key={index} card={card} />
         ))}
       </div>
     </main>

1b04e7f280975aad4fabbac99a79be3f88dda018
danielatwell2k5@msn.com <danielatwell2k5@msn.com>
2023-03-18 12:07:04 +0000

    fix: Aces check functionality

--- server/src/services/aces-check.ts
+++ server/src/services/aces-check.ts
@@ -3,7 +3,9 @@ import { Card } from "../types/game-types";
 export function checkAces(hand: Card[]) {
     // Find first ace with value of 11
     const aceIndex = hand.findIndex((card) => {
-        card.value === "ACE" && card.pointValue === 11
+        if (card.value === "ACE" && card.pointValue === 11) {
+            return true
+        }
     });
     // If found, replace pointValue with 1
     if (aceIndex !== -1) {

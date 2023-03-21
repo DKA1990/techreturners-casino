3514c20897ec83760c89ecad12423c0882329481
danielatwell2k5@msn.com <danielatwell2k5@msn.com>
2023-03-21 12:14:11 +0000

    fix: Dealer hand title and mobile first fixes

--- client/src/components/dealerHand.tsx
+++ client/src/components/dealerHand.tsx
@@ -6,7 +6,7 @@ export const DealerHand : React.FC = () => {
 
     return (
         <div>
-            {dealerCards.length > 1 && <p>Dealer Hand</p>}
+            {dealerCards.length > 0 && <p>Dealer Hand</p>}
             {dealerCards.length < 1 && 
                 <>
                     <img alt="face down card" src={require("../images/back_of_card.png")} className="card" />
